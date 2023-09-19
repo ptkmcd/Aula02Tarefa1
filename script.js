6bc8bcf8257f9614b465a67bf2d1599f716da066
Patrick Macedo <144183578+ptkmcd@users.noreply.github.com>
2023-09-19 19:57:17 -0300

    commit inicial
    Aula 2 Tarefa 1

---
+++ script.js
@@ -0,0 +1,8 @@
+// Seleciona o botão pelo ID
+const botaoAlerta = document.getElementById("botaoAlerta");
+
+// Adiciona um evento de clique ao botão
+botaoAlerta.addEventListener("click", function () {
+    // Exibe uma mensagem de alerta
+    alert("Mensagem de Alerta: Olá Mundo");
+});
